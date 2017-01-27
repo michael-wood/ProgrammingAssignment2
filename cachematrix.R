d1bbfd5c7d483867f3011a381ef3bb077cc9b669
Michael Wood <mchl.wood@gmail.com>
2017-01-27 13:40:49 -0500

    Updated cachematrix.R with Week 3 assignment

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,47 @@
-## Put comments here that give an overall description of what your
-## functions do
+#
+# makeCacheMatrix:  outputs a list of functions to get the inverse of a matrix
+# cacheSolve:       returns the cached or newly solved inverse
 
-## Write a short comment describing this function
+# makeCacheMatrix:
+#	INPUTS |  a square matrix which is assumed to be inversable
+#	OUTPUTS |  a list (cm) containing the functions:
+#			setmat - changes the input matrix
+#			getmat - returns the input matrix
+#			setinv - uses the solve function to find the invers
+#			getinv - returns the matrix inverse
+#
+#			functions from the list are referenced: cm$function
 
 makeCacheMatrix <- function(x = matrix()) {
+	minv <- NULL
+	setmat <- function(y) {
+                x <<- y
+                minv <<- NULL
+        }
+	getmat <- function() x
+	setinv <- function(solve) minv <<- solve
+	getinv <- function() minv
+	list(set = setmat, get = getmat,
+             setinv = setinv,
+             getinv = getinv)
 
 }
 
 
-## Write a short comment describing this function
+# cacheSolve:
+#	INPUTS |  the list vector created by the makeCacheMatrix function
+#	OUTPUTS |  the cached or newly solved inverse of the input matrix
 
 cacheSolve <- function(x, ...) {
+	minv <- x$getinv()
+	if(!is.null(minv)) {
+                message("Retrieving data from cache")
+                return(minv)
+        }
+	mraw <- x$get()
+	minv <- solve(mraw)
+	x$setinv(minv)
+	minv
         ## Return a matrix that is the inverse of 'x'
 }
+
